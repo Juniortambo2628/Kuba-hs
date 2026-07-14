58e988aa01421ef7334b0486050049b3bd66a397
Juniortambo2628 <100776648+Juniortambo2628@users.noreply.github.com>
2026-07-15 01:33:20 +0300

    Automate deployment via GitHub Actions

--- backend/public/index.php
+++ backend/public/index.php
@@ -8,15 +8,15 @@ define('LARAVEL_START', microtime(true));
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
-} elseif (file_exists($maintenance = __DIR__.'/../../../kuba_backend/storage/framework/maintenance.php')) {
+} elseif (file_exists($maintenance = __DIR__.'/../../kuba_backend/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
 $autoload = __DIR__.'/../vendor/autoload.php';
 if (!file_exists($autoload)) {
-    // Check production path (moved to public_html/api/public) relative to /home/ycpixrti/kuba_backend
-    $autoload = __DIR__.'/../../../kuba_backend/vendor/autoload.php';
+    // Check production path (moved to public_html/api) relative to /home/ycpixrti/kuba_backend
+    $autoload = __DIR__.'/../../kuba_backend/vendor/autoload.php';
 }
 require $autoload;
 
@@ -24,7 +24,7 @@ require $autoload;
 /** @var Application $app */
 $bootstrap = __DIR__.'/../bootstrap/app.php';
 if (!file_exists($bootstrap)) {
-    $bootstrap = __DIR__.'/../../../kuba_backend/bootstrap/app.php';
+    $bootstrap = __DIR__.'/../../kuba_backend/bootstrap/app.php';
 }
 $app = require_once $bootstrap;
 
